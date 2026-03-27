cae2255324046d5c37efba0e7be58c95208e3555
Vitalii Kiral <vitalii.kiral@comap-control.com>
2026-03-27 10:24:22 +0100

    Add MVC base

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,6 +1,18 @@
+using ToDoApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IToDoService, InMemoryToDoService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/tasks", (IToDoService service) => 
+{ 
+    return service.GetAllTasks(); }
+);
+app.MapPost("/tasks", (string title, IToDoService service) => 
+{ 
+    return service.AddTask(title); 
+});
 
 app.Run();
---
+++ ToDoApi/Services/InMemoryToDoService.cs
@@ -0,0 +1,18 @@
+﻿using ToDoApi.Models;
+
+namespace ToDoApi.Services;
+
+public class InMemoryToDoService : IToDoService
+{
+    private readonly Dictionary<Guid, ToDoItem> _tasks = new();
+
+    public ToDoItem AddTask(string title)
+    {
+        var genId = Guid.NewGuid();
+        var newTask = new ToDoItem(genId, title, false);
+        _tasks.Add(genId, newTask);
+        return newTask;
+    }
+
+    public List<ToDoItem> GetAllTasks() => _tasks.Values.ToList();
+}
